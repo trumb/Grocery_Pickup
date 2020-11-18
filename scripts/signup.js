795b3a54d0dffe4806ae3ea10a50120e05436a30
Zichun Xu <wudao.lilian@gmail.com>
2020-11-19 07:54:04 +0800

    changed color & style

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,20 +1,18 @@
 // The size of the cart. Number of items in the cart.
 var cartSize = 0;
 
-// Create a cart when the page is loaded.
-$(document).ready(function() {
-  createCart();
-});
-$("#submit").on("click", function(event) {
-  db.collection("account").doc().set({
-  email: document.getElementById("email").value,
-  firstname: document.getElementById("fname").value,
-  lastname: document.getElementById("lname").value,
-  phone: document.getElementById("num").value,
-  password: document.getElementById("password").value,
+$("#submit").on("click", function (event) {
+  console.log( document.getElementById("email").value);
+   db.collection("accounts").doc().set({
+     email: document.getElementById("email").value,
+    firstname: document.getElementById("fname").value,
+    lastname: document.getElementById("lname").value,
+    phone: document.getElementById("num").value,
+    password: document.getElementById("password").value,
+
   });
-            
+  location.href = "/signup_succeed.html"
   //db.collection("accounts").doc("EthanWinters").update({
   //fieldName: true / false
   // });
-  });
+});
