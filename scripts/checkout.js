cb165f3ef808cd0fa489b26e59fa323c55e1cc4b
Joshua Krauchi <jkrauchi@my.bcit.ca>
2020-11-19 14:12:59 -0800

    Massively overhauled checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,92 +1,120 @@
 // The size of the cart. Number of items in the cart.
 var cartSize = 0;
 
-// Create a cart when the page is loaded.
-$(document).ready(function() {
-  createCart();
-});
-
 // "Empty Cart" button handler.
 $("#empty").on("click", function(event) {
-  cartSize = 0;
   db.collection("cart").get().then(function(snap) {
     snap.forEach(function(doc) {
       db.collection("cart").doc(doc.id).delete().then();
       document.getElementById(doc.id).remove();
     });
   });
-  checkIfCartEmpty(0);
+  cartSize = 0;
+  checkIfCartEmpty(cartSize);
 });
 
-function createCart() {
-  db.collection("cart").get().then(function(snap) {
-    snap.forEach(function(doc) {
-      cartSize++;
-      var id = doc.id;
-      var img = doc.data().img_name;
-      var n = doc.data().name;
-      var p = doc.data().price;
-      var q = doc.data().quantity;
-      $("#cart").append("<div id='" + id + "' class='d-flex flex-row align-items-center border-bottom border-primary'></div>");
-      $("#" + id).append("<img class='img-fluid img-thumbnail' src='images/" + img + "' alt='" + n + "'/>");
-      $("#" + id).append("<div class='container d-flex flex-column'></div>");
-      $("#" + id + " div").append("<span class='d-flex justify-content-between'><h5>" + n + "</h5></span>");
-      $("#" + id + " span").append("<button type='button' id='remove' class='btn btn-primary'>×</button>");
-      $("#" + id + " div").append("<div class='d-flex justify-content-between'>$" + p + "</div>");
-      $("#" + id + " div div").append("<div class='btn-group' role='group'></div>");
-      $("#" + id + " .btn-group").append("<button id='plus' class='btn btn-primary'>+</button>");
-      $("#" + id + " .btn-group").append("<span id='" + id + "q'>" + q + "</span>");
-      $("#" + id + " .btn-group").append("<button id='minus' class='btn btn-primary'>−</button>");
-      $("#" + id + " #remove").on("click", function(event) {
-        unsubscribe();
-        removeClick(id);
-        cartSize--;
-        checkIfCartEmpty(cartSize);
-      });
-      $("#" + id + " #plus").on("click", function(event) {
-        plusClick(id);
-      });
-      $("#" + id + " #minus").on("click", function(event) {
-        minusClick(id);
-      });
-      checkIfCartEmpty(cartSize);
-      var unsubscribe = db.collection("cart").doc(id).onSnapshot(function(updated) {
-        if (cartSize == 0) {
-          unsubscribe();
-        }
-        else if (updated.data().quantity == 0) {
-          unsubscribe();
-          removeClick(id);
-          cartSize--;
-          checkIfCartEmpty(cartSize);
-        } else {
-          document.getElementById(id + "q").innerText = updated.data().quantity;
-        }
-      });
+const cart = document.querySelector("#cart");
+
+// Renders each item in the cart.
+function renderItem(doc) {
+  cartSize++;
+  let img_name = doc.data().img_name;
+  let n = doc.data().name;
+  let p = doc.data().price;
+  let q = doc.data().quantity;
+
+  let itemDiv = document.createElement("div");
+  let img = document.createElement("img");
+  let name = document.createElement("h5");
+  let remove = document.createElement("button");
+  let price = document.createElement("span");
+  let plus = document.createElement("button");
+  let quantity = document.createElement("span");
+  let minus = document.createElement("button");
+
+  itemDiv.setAttribute("data-id", doc.id);
+  itemDiv.classList.add("d-flex", "flex-row", "align-items-center", "border-bottom", "border-primary");
+  img.classList.add("img-fluid", "img-thumbnail");
+  img.src = "images/" + img_name;
+  img.alt = n;
+  name.classList.add("d-flex", "justify-content-between");
+  name.textContent = n;
+
+  price.textContent = "$" + p;
+  quantity.textContent = q;
+
+  remove.classList.add("btn", "btn-primary");
+  remove.textContent = "×";
+  plus.classList.add("btn", "btn-primary");
+  plus.textContent = "+";
+  minus.classList.add("btn", "btn-primary");
+  minus.textContent = "−";
+
+  itemDiv.appendChild(img);
+  itemDiv.appendChild(name);
+  itemDiv.appendChild(remove);
+  itemDiv.appendChild(price);
+  itemDiv.appendChild(plus);
+  itemDiv.appendChild(quantity);
+  itemDiv.appendChild(minus);
+
+  cart.appendChild(itemDiv);
+
+  // Remove (×) button handler. Removes item from cart.
+  remove.addEventListener("click", event => {
+    event.stopPropagation();
+    let id = event.target.parentElement.getAttribute("data-id");
+    db.collection("cart").doc(id).update({
+      quantity: 0
     });
   });
-}
 
-// "Remove" button handler.
-function removeClick(id) {
-  db.collection("cart").doc(id).delete().then();
-  document.getElementById(id).remove();
-}
+  // "Plus" button handler. Increments quantity.
+  plus.addEventListener("click", event => {
+    event.stopPropagation();
+    let id = event.target.parentElement.getAttribute("data-id");
+    const increment = firebase.firestore.FieldValue.increment(1);
+    db.collection("cart").doc(id).update({
+      quantity: increment
+    });
+  });
 
-// "Plus" button handler. Increments quantity.
-function plusClick(id) {
-  const increment = firebase.firestore.FieldValue.increment(1);
-  db.collection("cart").doc(id).update({
-    quantity: increment
+  // "Minus" button handler. Decrements quantity.
+  minus.addEventListener("click", event => {
+    event.stopPropagation();
+    let id = event.target.parentElement.getAttribute("data-id");
+    const decrement = firebase.firestore.FieldValue.increment(-1);
+    db.collection("cart").doc(id).update({
+      quantity: decrement
+    });
+  });
+
+  var onUpdate = db.collection("cart").doc(doc.id).onSnapshot(updated => {
+    if (cartSize == 0) {
+      onUpdate();
+    } else if (updated.data().quantity == 0) {
+      onUpdate();
+      removeItem(itemDiv);
+    } else {
+      quantity.textContent = updated.data().quantity;
+    }
   });
 }
 
-// "Minus" button handler. Decrements quantity.
-function minusClick(id) {
-  const decrement = firebase.firestore.FieldValue.increment(-1);
-  db.collection("cart").doc(id).update({
-    quantity: decrement
+// Sends each document (item) in the cart to renderItem().
+db.collection("cart").get().then(snap => {
+  snap.forEach(doc => {
+    renderItem(doc);
   });
+  checkIfCartEmpty(cartSize);
+});
+
+function removeItem(itemDiv) {
+  let id = itemDiv.getAttribute("data-id");
+  db.collection("cart").doc(id).delete();
+  itemDiv.remove();
+  cartSize--;
+  checkIfCartEmpty(cartSize);
 }
 
 // If cart size is greater than 0, enable proceed button. Else, disable it.
@@ -97,9 +125,3 @@ function checkIfCartEmpty(cs) {
     $("footer a").addClass("disabled");
   }
 }
-
-/*async getMarker() {
-    const snapshot = await firebase.firestore().collection('events').get()
-    return snapshot.docs.map(doc => doc.data());
-}
-*/
