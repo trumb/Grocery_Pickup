391b2b145280f0adaa084116be7df21280e60a2e
Eric Tan <etan34@my.bcit.ca>
2020-11-28 18:46:33 -0800

    firebase to profile

--- scripts/account.js
+++ scripts/account.js
@@ -1,25 +1,18 @@
-//---------------------------------------------------
-// This function checks to see if the user is sign in.
-// If so, then you can go to the "users" collection,
-// look for this person's document id (which would be authentication 
-// object ("user")'s uid, and get that document.
-// Now you can grab the name, or give a personalized greeting :)
-//----------------------------------------------------
 function getUser(){
-  firebase.auth().onAuthStateChanged(function (user) {
-      if (user){
-          console.log ("user is signed in");
-          db.collection("users")
-          .doc(user.uid)
-          .get()
-          .then(function(doc){
-              var n = doc.data().name;
-              console.log(n);
-              $("#username").text(n);
-          })
-      }
-      else {
-          console.log ("no user is signed in");
-      }
-  })
+    firebase.auth().onAuthStateChanged(function (users) {
+        if (users){
+            console.log ("user is signed in");
+            db.collection("users")
+            .doc(user.uid)
+            .get()
+            .then(function(doc){
+                var n = doc.data().first_name;
+                console.log(n);
+                $("#username").text(n);
+            })
+        }
+        else {
+            console.log ("no user is signed in");
+        }
+    })
 }
